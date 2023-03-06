97334ec74eb253f158342b8ccc5dba6a8f95b835
Brodys1 <123720584+Brodys1@users.noreply.github.com>
2023-03-06 13:43:02 -0800

    Q2

--- q1.py
+++ q1.py
@@ -1,6 +1,20 @@
 
 # ******************************
 # Make your Code
+flag = 0
+count = 0
+for i in range(10):
+    number = int(input("Enter a number: "))
+    if number % 2 == 0: # Even
+        flag += 1
+        if i == 9:
+            if flag >= 2:
+                count += 1
+    else: # Odd
+        if flag >= 2:
+            count += 1
+        flag = 0
+print(count)
 # ******************************
 
 
